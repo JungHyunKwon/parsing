017c50ea8b4b69cccce8965bbcd43357439abd90
JungHyunKwon <kjh_3859@naver.com>
2019-02-26 09:37:08 +0900

    published

--- js/parsing.js
+++ js/parsing.js
@@ -15,7 +15,7 @@ try {
 		 * @param {number} index
 		 * @return {string}
 		 */
-		window.parsing = function(value, from, to, index) {
+		/*window.parsing = function(value, from, to, index) {
 			var result = '';
 
 			//문자일 때
@@ -30,6 +30,46 @@ try {
 					//문자가 있을 때
 					if(toIndex > -1) {
 						result = value.substring(startIndex, toIndex);
+					}
+				}
+			}
+
+			return result;
+		};*/
+
+		/**
+		 * @name parsing
+		 * @since 2017-12-06
+		 * @param {string} value
+		 * @param {string} from
+		 * @param {string} to
+		 * @return {array}
+		 */
+		window.parsing = function(value, from, to) {
+			var result = [];
+			
+			//문자일 때
+			if(typeof value === 'string' && typeof from === 'string' && typeof to === 'string') {
+				let fromIndex = 0,
+					toIndex = 0,
+					startIndex = 0;
+				
+				while(value.indexOf(from, fromIndex) > -1) {
+					fromIndex = value.indexOf(from, fromIndex);
+					startIndex = fromIndex + from.length;
+					toIndex = value.indexOf(to, startIndex);
+					
+					//값이 있을 때
+					if(toIndex > -1) {
+						result.push({
+							value : value.substring(startIndex, toIndex),
+							fromIndex : startIndex,
+							toIndex : toIndex
+						});
+
+						fromIndex = toIndex + 1;
+					}else{
+						break;
 					}
 				}
 			}
